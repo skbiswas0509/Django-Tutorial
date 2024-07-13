1efb08fb808405e7d25801013af5b5c2387de7b3
Sukumar Biswas <skbiswas0509@gmail.com>
2024-07-13 12:33:19 +0600

    Till 3.50 hours

--- polls/views.py
+++ polls/views.py
@@ -5,18 +5,22 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render 
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html" 
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
